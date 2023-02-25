88badbef95f2a350a10bb0a18739536ae9275a77
soopertramp <pradeepchandrareddy95@gmail.com>
2023-02-25 17:32:28 +0530

    script

---
+++ day_1.py
@@ -0,0 +1,25 @@
+""" Question - 1 : Write a Python program to print "Hello Python" """
+
+print("Hello Python")
+
+""" Question - 2 : Write a Python program to do arithmetical operations addition
+and division """
+
+a = 10
+b = 5
+
+addition = a + b
+
+division = a / b
+
+print(f'The sum of {a} and {b} is {addition}.')
+print(f'The division of {a} and {b} is {division}.')
+
+""" Question - 3 : Write a Python program to find the area of triangle """
+
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+
+area = 0.5 * base * height
+
+print(f'The area of the triangle with {base} and {height} is {area}')
